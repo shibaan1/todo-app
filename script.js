67d84858d839cd6ad3dfbad21d11e8d3752ea339
Shibaan Askari <askarishibaan1@gmail.com>
2026-04-28 15:06:04 +0530

    feat: add done/undo toggle with color feedback

--- script.js
+++ script.js
@@ -2,37 +2,49 @@ const addtask = document.getElementById('addtask')
 const input = document.getElementById('input')
 const tasklist = document.getElementById('tasklist')
 
-addtask.addEventListener('click' , ()=>{
+addtask.addEventListener('click', () => {
 
     const inpval = input.value
 
-    if(inpval.trim() === ''){
+    if (inpval.trim() === '') {
         alert('no input')
     }
-     else{
-        let li = document.createElement('li') 
+    else {
+        let li = document.createElement('li')
         let dbtn = document.createElement('button')
         let rbtn = document.createElement('button')
-        dbtn.textContent = "done"
+        dbtn.textContent = "DONE"
         dbtn.dataset.action = 'done'
         rbtn.dataset.action = 'remove'
-        rbtn.textContent = 'remove'
-        li.textContent = inpval
-        li.appendChild(dbtn)
-        li.appendChild(rbtn)
+        rbtn.textContent = 'REMOVE'
+        const btnWrapper = document.createElement('div')
+        const taskText = document.createTextNode(inpval)
+        li.appendChild(taskText)
+        li.appendChild(btnWrapper)
+        btnWrapper.appendChild(dbtn)
+        btnWrapper.appendChild(rbtn)
         tasklist.appendChild(li)
         input.value = ''
-        
-     }
+
+    }
 })
 
-tasklist.addEventListener('click' , (e)=>{
+tasklist.addEventListener('click', (e) => {
 
-    if(e.target.dataset.action === 'done'){
-        e.target.parentElement.classList.add('done')
+    if (e.target.dataset.action === 'done') {
+        e.target.parentElement.parentElement.classList.toggle('done')
+
+        if(e.target.parentElement.parentElement.classList.contains('done')){
+
+            e.target.textContent = 'UNDO'
+        }
+        else{
+            e.target.textContent = "DONE"
+        }
+
     }
-    
-    if(e.target.dataset.action === 'remove'){
-        e.target.parentElement.remove()
+
+    if (e.target.dataset.action === 'remove') {
+        e.target.parentElement.parentElement.remove()
     }
 })
