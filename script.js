789097b498d56c6f576889d39e054b1b42be9214
Shibaan Askari <askarishibaan1@gmail.com>
2026-04-28 16:31:11 +0530

    feat: add filter button functionality

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const addtask = document.getElementById('addtask')
 const input = document.getElementById('input')
 const tasklist = document.getElementById('tasklist')
+const taskcompleted = document.getElementById("taskcompleted") 
+const totaltask = document.getElementById('totaltask')
+const filter = document.getElementById('filter')
+
 
 addtask.addEventListener('click', () => {
 
@@ -25,6 +29,7 @@ addtask.addEventListener('click', () => {
         btnWrapper.appendChild(rbtn)
         tasklist.appendChild(li)
         input.value = ''
+        updateCounter()
 
     }
 })
@@ -33,6 +38,7 @@ tasklist.addEventListener('click', (e) => {
 
     if (e.target.dataset.action === 'done') {
         e.target.parentElement.parentElement.classList.toggle('done')
+        updateCounter()
 
         if(e.target.parentElement.parentElement.classList.contains('done')){
 
@@ -46,5 +52,46 @@ tasklist.addEventListener('click', (e) => {
 
     if (e.target.dataset.action === 'remove') {
         e.target.parentElement.parentElement.remove()
+        updateCounter()
     }
+})
+
+function updateCounter(){
+    let totaltasks = tasklist.querySelectorAll('li').length
+    let completedtask = tasklist.querySelectorAll('li.done').length
+
+    taskcompleted.textContent = completedtask
+    totaltask.textContent = totaltasks
+}
+
+filter.addEventListener('click' , (e)=>{
+    const filtervalue = e.target.dataset.filter
+    const alltask = tasklist.querySelectorAll('li')
+
+    alltask.forEach(li =>{
+        if(filtervalue === 'active'){
+            if(li.classList.contains('done')){
+                li.style.display = 'none'
+            }
+
+            else{
+                li.style.display = 'flex'
+            }
+
+        }
+
+        if(filtervalue === 'completed'){
+            if(li.classList.contains('done')){
+                li.style.display = 'flex'
+            }
+            else{
+                li.style.display = 'none'
+            }
+        }
+
+        if(filtervalue === 'all'){
+            li.style.display = 'flex'
+
+        }
+    })
 })
